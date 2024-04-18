2e86826e24fee2e1527eab0dfaaf5ed303bbce30
huwei <1990huwei@sina.com>
2024-04-18 14:42:09 +0800

    1

--- blocks/position_encoder.py
+++ blocks/position_encoder.py
@@ -9,3 +9,15 @@ class PositionEncoder(nn.Module):
 
         position = torch.arange(max_seq_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-math.log(10000.0) / d_model))
+        pe = torch.zeros(max_seq_len, d_model)
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0).transpose(0, 1)
+
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        x = self.dropout(x)
+        return x
