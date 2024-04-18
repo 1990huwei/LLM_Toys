2444c978a0ef487f3486e14e9d813a3deadaca51
huwei <1990huwei@sina.com>
2024-04-18 10:33:33 +0800

    1

--- blocks/encoder.py
+++ blocks/encoder.py
@@ -9,3 +9,22 @@ class Encoder(nn.Module):
         super().__init__()
         self.multi_head_attention = MultiHeadAttention(d_model = d_model, n_heads=n_heads, dropout = dropout)
         self.feed_forward = FeedForward(d_model = d_model, d_ff=d_ff, dropout=dropout)
+
+        self.norm1 = nn.LayerNorm(d_model)
+        self.norm2 = nn.LayerNorm(d_model)
+
+        self.dropout1 = nn.Dropout(dropout)
+        self.dropout2 = nn.Dropout(dropout)
+
+    def forward(self, src, src_mask = None):
+        attention_output = self.multi_head_attention(src, src, src, mask = src_mask)
+        attention_output = self.dropout1(attention_output)
+        src = src + attention_output
+        src = self.norm1(src)
+
+        feedforward_output = self.feed_forward(src)
+        feedforward_output = self.dropout2(feedforward_output)
+        src = src + feedforward_output
+        src = self.norm2(src)
+
+        return src
