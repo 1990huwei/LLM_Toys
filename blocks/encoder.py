c8aaf3267cfc4cc809131e0d7225d3216cc75442
huwei <1990huwei@sina.com>
2024-04-18 10:42:37 +0800

    1

--- blocks/encoder.py
+++ blocks/encoder.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from attention import MultiHeadAttention
 from feedforward import FeedForward
 
-class Encoder(nn.Module):
+class EncoderLayer(nn.Module):
     def __init__(self, d_model, n_heads, d_ff = 2048, dropout = 0.1):
         super().__init__()
         self.multi_head_attention = MultiHeadAttention(d_model = d_model, n_heads=n_heads, dropout = dropout)
